52d6f867fa5c488842fbbdacfaad7aba73f938e0
Clinton Clark <clintonclark1995@hotmail.com>
2025-07-23 22:30:43 -0500

    app.js: handling for envelope crud routes, remove envelope logic

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import "dotenv/config"
 import express from "express"
 import { MiddlewareLogger } from "./middleware.js"
+import {
+  handlerDeleteEnvelope,
+  handlerGetEnvelopes,
+  handlerCreateEnvelope,
+  handlerGetEnvelopeByID,
+  handlerModifyEnvelope,
+} from "./envelope.js"
 const port = process.env.PORT || 3000
 
-const envelopes = ["personal", "rent"]
-
 const app = express()
+app.use(express.json())
 app.use(MiddlewareLogger)
-app.get("/envelopes", (req, res) => {
-  res.send(envelopes)
-})
-
-app.listen(port, () => {
+app.post("/envelopes", handlerCreateEnvelope)
+app.get("/envelopes", handlerGetEnvelopes)
+app.get("/envelopes/:id", handlerGetEnvelopeByID)
+app.patch("/envelopes/:id", handlerModifyEnvelope)
+app.delete("/envelopes/:id", handlerDeleteEnvelope)
+app.listen(port, (err) => {
+  if (err) console.log(`Error: ${err}`)
   console.log(`Application now running on port ${port}`)
 })
